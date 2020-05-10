c635079900c43c88a939dd5c1cd789f04132b2fb
Gustavo <github@gustavo.claroenigma.com.br>
2020-05-09 22:53:45 -0300

    Better code organisation

--- luar.lua
+++ luar.lua
@@ -31,21 +31,8 @@ kak = setmetatable({}, {
 	end
 })
 
-args = function() return table.unpack(arg) end
-
-local function compile(chunk)
-	local fn, err = load(chunk, "luar")
-
-	if fn then return fn end
-
-	err = err:match('%[string "luar"%]:(.+)')
-	local message = "error while parsing lua block:\n\nlua %%{%s}\n\nline %s\n"
-	debug(message:format(chunk, err))
-	os.exit(1)
-end
-
-local lambda = arg[#arg]
--- Remove reference to file's name and to lambda's code
+local chunk = arg[#arg]
+-- Remove reference to file's name and to chunk
 arg[0], arg[#arg] = nil, nil
 
 for i, v in ipairs(arg) do
@@ -58,22 +45,33 @@ for i, v in ipairs(arg) do
 	end
 end
 
-local function try(fn)
+args = function() return table.unpack(arg) end
+
+local function abort(action, err)
+	err = err:match('%[string "luar"%]:(.+)')
+	local message = "error while %s lua block:\n\nlua %%{%s}\n\nline %s\n"
+	debug(message:format(action, chunk, err))
+	os.exit(1)
+end
+
+local function compilechunk()
+	local fn, err = load(chunk, "luar")
+	if fn then return fn end
+	abort("parsing", err)
+end
+
+local function call(fn)
 	local check = function(success, ...)
 		local result = {...}
 		if success then return result end
-
-		local err = result[1]:match('%[string "luar"%]:(.+)')
-		local message = "error on execution of lua block:\n\nlua %%{%s}\n\nline %s\n"
-		debug(message:format(lambda, err))
-		os.exit(1)
+		abort("executing", result[1])
 	end
 
 	return check(pcall(fn))
 end
 
-local fn = compile(lambda)
-local result = try(fn)
+local fn = compilechunk()
+local result = call(fn)
 
 if #result > 0 then
 	print("echo " .. table.concat(result, "\t"))
