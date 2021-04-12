80ae7e4a61e3bcc06d40cfb033c6cd585f4c6232
Gustavo HMS <github@gustavo.claroenigma.com.br>
2021-04-11 21:52:13 -0300

    Make returned values replace selections

--- luar.lua
+++ luar.lua
@@ -24,24 +24,26 @@ if arg[1] == "-debug" then
 	table.remove(arg, 1)
 end
 
+local function quote(words)
+	for i, v in ipairs(words) do
+    	words[i] = string.format("%%☾%s☾", v)
+	end
+
+	return table.concat(words, " ")
+end
+
+args = function() return table.unpack(arg) end
+
 kak = setmetatable({}, {
 	__index = function(t, command)
 		local name = command:gsub("_", "-")
 		t[command] = function(...)
-			local words = { name }
-
-			for _, v in ipairs {...} do
-				words[#words + 1] = string.format("%%☾%s☾", v)
-			end
-
-			write(table.concat(words, " "))
+			write(name .. " " .. quote {...})
 		end
 
 		return t[command]
 	end
 })
-
-args = function() return table.unpack(arg) end
 
 local chunk = arg[#arg]
 arg[0], arg[#arg] = nil, nil -- Hide file name and chunk
@@ -64,5 +66,11 @@ if not results[1] then abort("executing", chunk, results[2]) end
 
 if #results > 1 then
 	table.remove(results, 1)
-	print("echo " .. table.concat(results, "\t"))
+	local command = string.format([[
+		evaluate-commands -save-regs dquote %%{
+        	set-register dquote %s
+        	execute-keys R
+		}
+	]], quote(results))
+	print(command)
 end
